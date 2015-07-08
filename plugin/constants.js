8833ff3f486199051d26ee8c1a08f1b83d4ef300
Nicolas Siver <nicolas@siver.im>
2015-07-08 21:34:50 +0300

    add constants

---
+++ plugin/constants.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Nicolas on 7/8/15.
+ */
+module.exports = Object.freeze({
+    'GLOBAL_AWARD_COUNTER': 'nextNsMetaTag',
+    'NAMESPACE'           : 'ns:meta_tag',
+    'SOCKETS'             : 'ns-meta-tag',
+    'WIDGET'              : 'ns_meta_tag'
+});
